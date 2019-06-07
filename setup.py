6b9dba602c3acc3db2e5d762b59a1df354fdc234
Francois Delarbre <francoisdelarbre@iot-d.com>
2019-06-07 10:32:39 +0000

    prepare version 2.2.5a1

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ and is distributed under the MIT license.
 '''
 
 setup(name='Keras',
-      version='2.2.4',
+      version='2.2.5a1',
       description='Deep Learning for humans',
       long_description=long_description,
       author='Francois Chollet',
